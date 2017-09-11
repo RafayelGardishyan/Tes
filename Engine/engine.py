d561cdad56d5433ad01b91f6c394bb25670540bd
RafayelGardishyan <rgardishyan@gmail.com>
2017-09-11 07:14:27 +0200

    First Commit

---
+++ Engine/engine.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+import datetime as dt
+from gtts import gTTS
+from playsound import playsound
+import os
+import pyowm
+import json
+import urllib3
+
+class Assistant:
+
+
+    count = 0
+
+    def get_responses(self):
+        return {
+            'hello': {self.speak: 'Hi'},
+            'hi': {self.speak: 'Hello'},
+            'how are you': {self.speak: 'Very well!'},
+            'what is your name': {self.speak: 'My name is RafAssistant'},
+            'what is your age': {self.speak: 'I am ' + str(dt.date.today().year - 2017) + " years old."},
+            'how old are you': {self.speak: 'I am ' + str(dt.date.today().year - 2017) + " years old."},
+            'what is today\'s date': {
+                self.speak: 'Today is ' + str(dt.date.today().day) + '.' + str(dt.date.today().month) + '.' + str(
+                    dt.date.today().year) + '.'},
+            'bye bye': {self.speak: 'Bye-Bye!',
+                        self.exit_app: 'None'},
+            'where are you born': {self.speak: 'I am born in The Netherlands'},
+            'fine to meet you': {self.speak: 'Nice to meet you, too'},
+            'do you like YouTube': {self.speak: 'Yes, I love it!'},
+            'what is the weather now': {self.speak: 'It is now ' + self.get_weatherO() + ' degrees'},
+            'search': {self.speak: 'Ok, what would you like to search?'},
+            'ok Google': {self.speak: 'Ha Ha Ha, Very funny!'},
+            'what is the meaning of life': {self.speak: '42.'},
+            'I\'m drunk': {self.speak: 'I hope you\'re not driving now!'},
+            'tell me a joke': {self.speak: 'If you understand English, press 1. If you do not understand English, press 2.'},
+            'job': {self.speak: 'What would you like to add to your task list?', self.add_task: 'Jobs'},
+            'get jobs': {self.speak: 'Here are your tasks!', self.get_tasks: 'Jobs'},
+            'delete job': {self.speak: 'What is the name of the task you want to delete?', self.delete_task: 'Jobs'}
+        }
+
+    def speak(self, phrase):
+        tts = gTTS(text=phrase, lang='en')
+        tts.save('data/voice/response' + str(self.count) + '.mp3')
+        playsound('data/voice/response' + str(self.count) + '.mp3')
+        self.count += 1
+        print(phrase + "\n")
+
+
+    def look_for_answer(self, user_input, responses_list):
+        for key in responses_list:
+            if user_input == key:
+                for action in responses_list[key]:
+                    action(responses_list[key][action])
+        return True
+
+    def add_task(self, phrase = None):
+        task = self.record()
+        with open('data/tasks.txt', 'a+') as f:
+            f.write(task + '\n')
+        self.speak('Added \'' + task + '\' to your task list')
+
+    def get_tasks(self, phrase=None):
+        with open('data/tasks.txt', 'r+') as f:
+            tasks = f.readlines()
+            tasks = [x.strip() for x in tasks]
+            for item in tasks:
+                self.speak(item)
+
+    def delete_task(self, phrase=None):
+        task = self.record()
+        fn = 'data/tasks.txt'
+        f = open(fn)
+        output = []
+        for line in f:
+            if task != line.strip():
+                output.append(line)
+        f.close()
+        f = open(fn, 'w')
+        f.writelines(output)
+        f.close()
+        self.speak('Successfully removed \'' + task + '\'from your task list')
+
+    def record(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            text = "....."
+        except sr.RequestError as e:
+            text = ""
+            print("Could not request results; {0}".format(e))
+
+        # print(text)
+        return text
+
+
+    def exit_app(self, Bye = None):
+        quit()
+
+    def search(self, tag):
+        x = 0
+        print("Searching for " + tag + "\n")
+        global count
+        http = urllib3.PoolManager()
+        url = 'http://api.duckduckgo.com/?q=' + tag + '&format=json'
+        response = http.request('GET', url)
+        data = json.loads(response.data)
+        self.speak('Here are some results!')
+        count += 1
+        for i in data["RelatedTopics"]:
+            if x > 5:
+                break
+            try:
+                resultText = data["RelatedTopics"][x]['Text']
+                self.speak(resultText)
+            except KeyError:
+                pass
+            count += 1
+            x += 1
+
+
+    # search('Python')
+    def get_weatherO(self, phrase = None):
+        http = urllib3.PoolManager()
+        url = 'http://ipinfo.io/json'
+        response = http.request('GET', url)
+        data = json.loads(response.data)
+
+        IP = data['ip']
+        org = data['org']
+        city = data['city']
+        country = data['country']
+        region = data['region']
+
+        owm = pyowm.OWM('be6c5ecdf84f55df9764465d1725b7ce')
+        observation = owm.weather_at_place(city + ',' + country.lower())
+        w = observation.get_weather()
+
+        return str(int(w.get_temperature('celsius')['temp']))
+
+
+
+    def mainloop(self):
+        while True:
+            self.look_for_answer(self.record(), self.get_responses())
---
+++ main.py
@@ -0,0 +1,5 @@
+from Engine import Assistant
+
+if __name__ == '__main__':
+    app = Assistant()
+    app.mainloop()
